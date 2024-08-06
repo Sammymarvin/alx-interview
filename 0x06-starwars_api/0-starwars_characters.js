2054fa2b3a681161f626228c29f5c6e071a0e667
sammymarvin <sammygatonye0@gmail.com>
2024-08-06 15:23:29 -0700

    dfg

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,42 +1,25 @@
-#!/usr/bin/env python3
+#!/usr/bin/node
+const request = require('request');
+const movieId = process.argv[2];
+const options = {
+  url: 'https://swapi-api.hbtn.io/api/films/' + movieId,
+  method: 'GET'
+};
 
-import requests
-import sys
+request(options, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
+  }
+});
 
-def fetch_characters(movie_id):
-    """
-    Fetch and print character names from the specified Star Wars movie ID.
-
-    Args:
-        movie_id (int): The ID of the Star Wars movie.
-    """
-    # Star Wars API URL for films
-    url = f"https://swapi.dev/api/films/{movie_id}/"
-    response = requests.get(url)
-
-    if response.status_code != 200:
-        print(f"Failed to retrieve data for movie ID {movie_id}")
-        return
-
-    movie_data = response.json()
-    characters = movie_data.get("characters", [])
-
-    for character_url in characters:
-        char_response = requests.get(character_url)
-        if char_response.status_code == 200:
-            char_data = char_response.json()
-            print(char_data.get("name"))
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: ./starwars_characters.py <movie_id>")
-        sys.exit(1)
-
-    try:
-        movie_id = int(sys.argv[1])
-    except ValueError:
-        print("Movie ID must be an integer")
-        sys.exit(1)
-
-    fetch_characters(movie_id)
-
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
